ca1bb163db94b5b66b9d14576bc2402ba125abd8
tomfenno <u1466976@umail.utah.edu>
2024-10-19 21:46:51 -0600

    Fixed typos in mcts code. Downloaded and modified .py file from GitHub to use as a game state for my algo.

--- mcts.py
+++ mcts.py
@@ -24,14 +24,17 @@ class Node():
 def mcts(initial_state, time_limit=None, iteration_limit=None):
     root = Node(initial_state, None)
     if time_limit:
-        while time.time() < time_limit:
+        end_time = time.time() + time_limit
+        while time.time() < end_time:
             playout(root)
     else:
         count = 0
         while count < iteration_limit:
             playout(root)
             count += 1
+
     best_child = most_visited(root)
+
     for action, child in root.children.items():
         if child == best_child:
             return action
@@ -46,17 +49,18 @@ def select(node):
         
 def expand(node):
     actions = node.state.get_actions()
+
     for action in actions:
         if action not in node.children:
-            new_node = Node(node.state.take_action(), node)
+            new_node = Node(node.state.take_action(action), node)
             node.children[action] = new_node
             return new_node
         
 def simulate(state):
     while not state.is_terminal():
         action = random.choice(state.get_actions())
-        node_state = state.take_action()
-        return node_state.get_utility()
+        node_state = state.take_action(action)
+    return node_state.get_utility()
     
 def backpropogate(node, utility):
     while node is not None:
@@ -72,35 +76,38 @@ def playout(node):
 def select_child(node):
     highest_UCB1 = float("-inf")
     best_nodes = []
+
     for child in node.children.values():
         ucb1 = (child.utility / child.visits) + C * math.sqrt(math.log(node.visits) / child.visits)
         if ucb1 > highest_UCB1:
             highest_UCB1 = ucb1
-            best_nodes[child]
+            best_nodes = [child]
         elif ucb1 == highest_UCB1:
             best_nodes.append(child)
-        return random.choice(best_nodes)
+    
+    return random.choice(best_nodes)
 
 def most_visited(node):
     most_visits = 0
-    ratio = 0
-    best_ratio = 0
     best_nodes = []
+
     for child in node.children:
-        if child.vists > most_visits:
+        if child.visits > most_visits:
             most_visits = child.visits
-            best_nodes[child]
+            best_nodes = [child]
         elif child.visits == most_visits:
             best_nodes.append(child)
         if len(best_nodes) == 1:
             return best_nodes[0]
         else:
-            best_nodes.clear
+            best_nodes.clear()
+            best_ratio = float("-inf")
             for node in best_nodes:
                 ratio = node.utility / node.visits
                 if ratio > best_ratio:
                     best_ratio = ratio
-                    best_nodes[node]
+                    best_nodes = [node]
                 elif ratio == best_ratio:
                     best_nodes.append(node)
-                return random.choice(best_nodes)
+    
+    return random.choice(best_nodes)
